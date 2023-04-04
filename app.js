1bb5c0d4ef3c198c07e18cc29dc996094f1ebaa8
ShivamPatel010902 <shivampatel010902@gmail.com>
2023-04-04 19:00:10 +0530

    ecommerce backend cors error

--- app.js
+++ app.js
@@ -13,12 +13,7 @@ import uploadRouter from './routers/uploadRouter.js';
 dotenv.config();
 
 const app = express();
-const corsOptions ={
-  origin:'https://onlineshopsp.netlify.app/', 
-  credentials:true,            
-  optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
